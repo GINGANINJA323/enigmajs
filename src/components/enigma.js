b41d81e6e0ec6d0d1ad09f26de850ec8e0f75f0e
Ed <edward82009@hotmail.com>
2020-06-08 19:18:11 +0100

    Removed redundant component imports

--- src/components/enigma.js
+++ src/components/enigma.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Tabview from './tabview';
 import Plugboard from './plugboard';
 import TextEntry from './textentry';
--- src/components/tabview.js
+++ src/components/tabview.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class Tabview extends React.Component {
   render() {
