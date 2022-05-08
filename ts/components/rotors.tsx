be66ffd05e2fc0fbcb763f17c2ae6e13deb98b06
Edward Glendinning <edward82009@hotmail.com>
2022-05-08 14:47:29 +0100

    Refactor rotors

---
+++ ts/components/rotors.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import Dropdown from './dropdown';
+const rotorSelection = require('./rotorSelect.json');
+import { alphabet } from './utils/utils';
+
+interface RotorConfigProps {
+  rotors: string[];
+  rotorStart: number[];
+  onChangeRotorType: (rotors: string[]) => void;
+  onChangeRotorStart: (rotorStart: number[]) => void;
+}
+
+const RotorConfig = (props: RotorConfigProps): JSX.Element => {
+  const [rotorArray, setRotorArray] = React.useState(props.rotors || ['I', 'II', 'III']);
+  const [rotorStart, setRotorStart] = React.useState(props.rotorStart || [0, 0, 0]);
+
+  const handleRotorChange = (value: string, pos: number): void => {
+    let newRotors = rotorArray;
+    newRotors[pos] = value;
+
+    setRotorArray(newRotors);
+  }
+
+  const handleStartChange = (value: number, pos: number): void => {
+    let newRotorStart = rotorStart;
+    newRotorStart[pos] = value;
+
+    setRotorStart(newRotorStart);
+  }
+
+  const handleChangeRotors = (): void => {
+    props.onChangeRotorType(rotorArray);
+    props.onChangeRotorStart(rotorStart);
+  }
+
+  const rotors = rotorSelection;
+
+  return (
+    <>
+      <h2>{'Rotor Selection'}</h2>
+      <p>{'Select the three rotors you would like to encrypt with, as well as their respective start positions.'}</p>
+
+      <Dropdown
+        onClick={(e) => handleRotorChange(e.target.value, 0)}
+        options={Object.keys(rotors)}
+        value={props.rotors[0]}
+      />
+      <input
+        type={'number'}
+        min={0}
+        max={26}
+        value={rotorStart[0]}
+        onChange={(e) => setRotorStart([Number(e.target.value), rotorStart[1], rotorStart[2]])}
+      />
+
+      <Dropdown
+        onClick={(e) => handleRotorChange(e.target.value, 1)}
+        options={Object.keys(rotors)}
+        value={props.rotors[1]}
+      />
+      <input
+        type={'number'}
+        min={0}
+        max={26}
+        value={rotorStart[1]}
+        onChange={(e) => setRotorStart([rotorStart[0], Number(e.target.value), rotorStart[2]])}
+      />
+
+      <Dropdown
+        onClick={(e) => handleRotorChange(e.target.value, 2)}
+        options={Object.keys(rotors)}
+        value={props.rotors[2]}
+      />
+      <input
+        type={'number'}
+        min={0}
+        max={26}
+        value={rotorStart[2]}
+        onChange={(e) => setRotorStart([rotorStart[0], rotorStart[1], Number(e.target.value)])}
+      />
+
+      <button
+        onClick={handleChangeRotors}
+      >{'Set'}</button>
+    </>
+  );
+
+}
+
+export default RotorConfig;
